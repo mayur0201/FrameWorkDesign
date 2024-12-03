7b22cf22de01bad7019575ae160915ebaa24ce48
Mayur Mangal <mayur.mangal.0201@gmail.com>
2024-12-03 17:39:07 +0530

    Added cmd command to run code through azure pipeline

--- src/test/java/org/pageobjects/setUpDockerGridTest.java
+++ src/test/java/org/pageobjects/setUpDockerGridTest.java
@@ -8,17 +8,31 @@ import java.io.IOException;
 
 public class setUpDockerGridTest {
 
+//    @BeforeTest
+//    public void startGrid() throws IOException, InterruptedException {
+//        Runtime.getRuntime().exec("cmd /c start start_dockerGrid.bat");
+//        Thread.sleep(15000);
+//    }
+//
+//    @AfterTest
+//    public void stopGrid() throws IOException, InterruptedException {
+//        Runtime.getRuntime().exec("cmd /c start stop_dockerGrid.bat");
+//        Thread.sleep(5000);
+//
+//        Runtime.getRuntime().exec("taskkill /f /im cmd.exe");
+//    }
+
+
     @BeforeTest
     public void startGrid() throws IOException, InterruptedException {
-        Runtime.getRuntime().exec("cmd /c start start_dockerGrid.bat");
-        Thread.sleep(15000);
+        Runtime.getRuntime().exec("sh ./start_dockerGrid.sh");
+        Thread.sleep(15000); // Adjust sleep time as needed
+    }
+        @AfterTest
+        public void stopGrid() throws InterruptedException, IOException {
+            Runtime.getRuntime().exec("sh ./stop_dockerGrid.sh");
+            Thread.sleep(5000);
+        }
+
     }
 
-    @AfterTest
-    public void stopGrid() throws IOException, InterruptedException {
-        Runtime.getRuntime().exec("cmd /c start stop_dockerGrid.bat");
-        Thread.sleep(5000);
-
-        Runtime.getRuntime().exec("taskkill /f /im cmd.exe");
-    }
-}
